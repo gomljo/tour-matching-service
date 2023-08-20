2d9086c6f8dbab8f8f62030bf5cf627a468af628
gomljo <gomljo0307@naver.com>
2023-08-20 11:00:08 +0900

    docs: 프로젝트 설명 문서 작성
    해외 배낭 여행 가이드 매칭 서비스에 대한 설명 문서 작성
    
    - 프로젝트 개요
    - 프로젝트 개발 환경
    - 커밋 메세지 규칙
    - ERD
    - 구현 예정 기능

---
+++ src/main/java/com/backpacking/BackpackingApplication.java
@@ -0,0 +1,13 @@
+package com.backpacking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackpackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackpackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backpacking/BackpackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backpacking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackpackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
