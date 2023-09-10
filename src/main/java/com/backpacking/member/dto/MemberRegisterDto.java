c36864cd564b3f73018d8ce1f897d95840363fc2
gomljo <gomljo0307@naver.com>
2023-09-11 02:11:20 +0900

    feat: 회원 가입 요청 객체 검증 어노테이션 추가
    회원 가입 요청 값 검증을 위한 검증 어노테이션 추가

--- src/main/java/com/backpacking/member/dto/MemberRegisterDto.java
+++ src/main/java/com/backpacking/member/dto/MemberRegisterDto.java
@@ -2,6 +2,11 @@ package com.backpacking.member.dto;
 
 import com.backpacking.member.domain.model.Member;
 import com.backpacking.member.domain.vo.Address;
+import com.backpacking.member.validation.register.address.ValidAddress;
+import com.backpacking.member.validation.register.email.Email;
+import com.backpacking.member.validation.register.password.Password;
+import com.backpacking.member.validation.register.phonenumber.Phone;
+import com.backpacking.member.validation.register.roles.Roles;
 import lombok.*;
 
 import java.util.List;
@@ -15,14 +20,19 @@ public class MemberRegisterDto {
     public static class Request {
 
         @NonNull
+        @Email
         private String email;
         @NonNull
+        @Phone
         private String phoneNumber;
         @NonNull
+        @Password
         private String password;
         @NonNull
+        @Roles
         private List<String> roles;
         @NonNull
+        @ValidAddress
         private Address address;
     }
 
@@ -30,7 +40,7 @@ public class MemberRegisterDto {
     @AllArgsConstructor
     @Getter
     public static class Response {
-        private long touristId;
+        private long memberId;
         private String email;
 
         public static Response fromEntity(Member member){
